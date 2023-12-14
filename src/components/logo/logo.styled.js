87eff51a85a55ddd7ede94e58bf6b6c0e7bd9d9a
Davit Jabushanuri <davitjabushanuri95@gmail.com>
2023-12-14 23:48:05 +0400

    feature/header
    create and style logo component

---
+++ src/components/logo/logo.styled.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+
+export const StyledLogo = styled(Link)`
+  color: var(--clr-primary);
+  background-color: var(--clr-titan-white);
+  font-size: var(--fs-h3);
+  font-weight: var(--fw-bold);
+  padding: 0.75em;
+  border-radius: 100vmax;
+  aspect-ratio: 1/1;
+  display: grid;
+  place-items: center;
+  line-height: 0;
+  transition: background-color 0.2s ease-in-out;
+`;
