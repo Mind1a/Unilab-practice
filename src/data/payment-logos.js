cdf63d0ec932286f615cf708139e19f500f07ace
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2023-12-18 05:21:22 +0400

    feature/footer
    adding payment logos and their data array

---
+++ src/data/payment-logos.js
@@ -0,0 +1,9 @@
+import logo_0 from "../assets/payment-logos/payment-logos-0.svg";
+import logo_1 from "../assets/payment-logos/payment-logos-1.svg";
+import logo_2 from "../assets/payment-logos/payment-logos-2.svg";
+import logo_3 from "../assets/payment-logos/payment-logos-3.svg";
+import logo_4 from "../assets/payment-logos/payment-logos-4.svg";
+import logo_5 from "../assets/payment-logos/payment-logos-5.svg";
+import logo_6 from "../assets/payment-logos/payment-logos-6.svg";
+
+export const payment_logos = [logo_0, logo_1, logo_2, logo_3, logo_4, logo_5, logo_6];
